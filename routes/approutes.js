dc4de72ce4158bbb01df0653c2a84a35e46230b9
DarkPixel100 <diegofavila20@gmail.com>
2023-10-01 05:08:09 -0300

    Novas rotas (nova pág. de admin e funcionalidades)

--- routes/approutes.js
+++ routes/approutes.js
@@ -2,15 +2,25 @@ const express = require("express");
 const router = express.Router();
 var viewController = require("../controllers/viewcontroller");
 var authController = require("../controllers/authcontroller");
+var generalController = require("../controllers/generalcontroller");
 
-router.get("/", viewController.showHome);
 router.get("/login", viewController.showLogin);
 router.get("/cadastro", viewController.showCadastro);
-
-// router.post("/", viewController.alugar);
 
 router.post("/cadastro", authController.criarUsuario);
 router.post("/login", authController.attemptLogin);
 router.post("/logout", authController.logout);
+
+router.get("/", viewController.showHome);
+router.post("/", viewController.showHome);
+router.get("/admin", viewController.showAdmin);
+router.post("/admin", viewController.showAdmin);
+
+router.post("/addLivro", generalController.addLivro);
+router.post("/alugarLivro", generalController.alugarLivro);
+router.post("/devolverLivro", generalController.devolverLivro);
+
+router.post("/deletarLivro", generalController.deletarLivro);
+router.post("/deletarUser", generalController.deletarUser);
 
 module.exports = router;
