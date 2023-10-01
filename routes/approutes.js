022a710afb8d0915077da68205a8e004e2a59905
DarkPixel100 <diegofavila20@gmail.com>
2023-10-01 17:46:14 -0300

    Validação com Validator e Patterns

--- routes/approutes.js
+++ routes/approutes.js
@@ -1,22 +1,47 @@
 const express = require("express");
 const router = express.Router();
-var viewController = require("../controllers/viewcontroller");
-var authController = require("../controllers/authcontroller");
-var generalController = require("../controllers/generalcontroller");
+const viewController = require("../controllers/viewcontroller");
+const authController = require("../controllers/authcontroller");
+const generalController = require("../controllers/generalcontroller");
+const { body } = require("express-validator");
 
 router.get("/login", viewController.showLogin);
 router.get("/cadastro", viewController.showCadastro);
 
-router.post("/cadastro", authController.criarUsuario);
-router.post("/login", authController.attemptLogin);
-router.post("/logout", authController.logout);
+router.post(
+  "/cadastro",
+  [
+    body("nome").isString().notEmpty(),
+    body("email").isEmail(),
+    body("password")
+      .isLength({ min: 8 }) // Minimum length of 8 characters
+      .matches(/[A-Z]/),
+  ],
+  authController.criarUsuario
+);
+router.post(
+  "/login",
+  [body("email").isEmail(), body("password").notEmpty()],
+  authController.attemptLogin
+);
+router.get("/logout", authController.logout);
 
 router.get("/", viewController.showHome);
 router.post("/", viewController.showHome);
 router.get("/admin", viewController.showAdmin);
 router.post("/admin", viewController.showAdmin);
 
-router.post("/addLivro", generalController.addLivro);
+router.post(
+  "/addLivro",
+  [
+    body("titulo").isString().notEmpty(),
+    body("autores").isString().notEmpty(),
+    body("ano").isInt().notEmpty(),
+    body("editora").isString().notEmpty(),
+    body("quantidade").isInt().notEmpty(),
+  ],
+  generalController.addLivro
+);
 router.post("/alugarLivro", generalController.alugarLivro);
 router.post("/devolverLivro", generalController.devolverLivro);
 
